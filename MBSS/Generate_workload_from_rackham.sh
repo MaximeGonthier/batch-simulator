8bbd87f22c1592f7f08eea477a347f4962ec31c3
Maxime <maxime.gonthier@ens-lyon.fr>
2022-04-22 12:20:59 +0200

    rewriting a bit the model

--- MBSS/Generate_workload_from_rackham.sh
+++ MBSS/Generate_workload_from_rackham.sh
@@ -2,10 +2,10 @@
 # bash Generate_workload_from_rackham.sh date_input_file
 
 # Get job history
-#~ echo "Downloading job history of" $1 "..."
-#~ scp maxim@rackham.uppmax.uu.se:../../../sw/share/slurm/rackham/accounting/$1 /home/gonthier/data-aware-batch-scheduling/MBSS/inputs/workloads/raw/$1
-#~ echo "Download done! Here are the first few lines of the raw workload" $1
-#~ head -5 inputs/workloads/raw/$1
+echo "Downloading job history of" $1 "..."
+scp maxim@rackham.uppmax.uu.se:../../../sw/share/slurm/rackham/accounting/$1 /home/gonthier/data-aware-batch-scheduling/MBSS/inputs/workloads/raw/$1
+echo "Download done! Here are the first few lines of the raw workload" $1
+head -5 inputs/workloads/raw/$1
 
 # Convert file into readable information and add input data
 echo "Converting job history..."
