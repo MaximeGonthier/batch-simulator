4596d750be32244160a45fcc16ea2c83bfe58c93
Maxime <maxime.gonthier@ens-lyon.fr>
2022-05-10 16:43:47 +0200

    new code for 3 phases

--- MBSS/Generate_workload_from_rackham.sh
+++ MBSS/Generate_workload_from_rackham.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-# bash Generate_workload_from_rackham.sh date_input_file
+# Merge consecutive files and do a workload that my simulator can read
+# bash Generate_workload_from_rackham.sh input_files
 
 # Get job history
-echo "Downloading job history of" $1 "..."
-scp maxim@rackham.uppmax.uu.se:../../../sw/share/slurm/rackham/accounting/$1 /home/gonthier/data-aware-batch-scheduling/MBSS/inputs/workloads/raw/$1
-echo "Download done! Here are the first few lines of the raw workload" $1
-head -5 inputs/workloads/raw/$1
+#~ echo "Downloading job history of" $1 "..."
+#~ scp maxim@rackham.uppmax.uu.se:../../../sw/share/slurm/rackham/accounting/$1 /home/gonthier/data-aware-batch-scheduling/MBSS/inputs/workloads/raw/$1
+#~ echo "Download done! Here are the first few lines of the raw workload" $1
+#~ head -5 inputs/workloads/raw/$1
 
 # Convert file into readable information and add input data
+echo "Merging input files..."
+echo "There are $# input files"
+FILE_START=$1
+FILE_END=${@: -1}
+START=${FILE_START:21}
+END=${FILE_END:21}
+OUTPUT="inputs/workloads/raw/"$START"->"$END
+echo "Output will be" $OUTPUT
+truncate -s 0 $OUTPUT
+for ((i=1; i<=$#; i++))
+do
+	echo "Adding" ${@:$i:1}
+	cat ${@:i:i} >> $OUTPUT
+done
+
 echo "Converting job history..."
-python3 src/generate_workload_from_rackham.py $1 10 10
-echo "Conversion done! Here are the first few lines of the converted workload" $1
-head -5 inputs/workloads/converted/$1
+python3 src/generate_workload_from_rackham.py $START"->"$END 10 10
+echo "Conversion done!"
--- MBSS/src/generate_workload_from_rackham.py
+++ MBSS/src/generate_workload_from_rackham.py
@@ -1,8 +1,7 @@
 # Read job history files from /sw/share/slurm/rackham/accounting in the Rackham cluster
 # Create jobs copying exactly these data
 # Add data inputs depending on the arguments given by the user
-# Can be used with an option to take into consideration 3 inputs files instead of 1.
-# With 3 inputs file, they are concatenated and input files 1 and 3 won't be counted later by the scheduler.
+# Divide in three the jobs sorted by submission time so you have 3 phases and can ignore phases 1 and 3.
 
 # Imports
 import sys
@@ -26,36 +25,36 @@ class Job:
 FILENAME = sys.argv[1] # FILE 1
 PROBABILITY_OF_USING_256GB = int(sys.argv[2]) # 0-100
 PROBABILITY_OF_USING_1TB = int(sys.argv[3]) # 0-100
-MERGE_3_FILES = int(sys.argv[4]) # 0 or 1
+# ~ MERGE_3_FILES = int(sys.argv[4]) # 0 or 1
 
-if (MERGE_3_FILES == 1):
-	FILENAME_BEFORE = sys.argv[5] # FILE before main file
-	FILENAME_AFTER = sys.argv[6] # FILE after main file
-	f_input_before = open("inputs/workloads/raw/" + FILENAME_BEFORE, "r")
-	f_input_after = open("inputs/workloads/raw/" + FILENAME_AFTER, "r")
+# ~ if (MERGE_3_FILES == 1):
+	# ~ FILENAME_BEFORE = sys.argv[5] # FILE before main file
+	# ~ FILENAME_AFTER = sys.argv[6] # FILE after main file
+	# ~ f_input_before = open("inputs/workloads/raw/" + FILENAME_BEFORE, "r")
+	# ~ f_input_after = open("inputs/workloads/raw/" + FILENAME_AFTER, "r")
 
 f_input = open("inputs/workloads/raw/" + FILENAME, "r")
 
 id_count = 1
 workload = []	
 
-# Read the file first to get a struct to sort by submission time
-if (MERGE_3_FILES == 1):
-	line = f_input_before.readline()
-	while line:
-		r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15 = line.split()
-		if (str(r3) == "jobstate=COMPLETED" and int(str(r11)[6:]) <= 20):
-			if (len(str(r15)) == 17): # Mean that the walltime is superior to 10 days
-				walltime = int(str(r15)[6:8])*24*60*60 + int(str(r15)[9:11])*60*60 + int(str(r15)[12:14])*60 + int(str(r15)[15:17])
-			if (len(str(r15)) == 16): # Mean that the walltime is superior to 24h
-				walltime = int(str(r15)[6:7])*24*60*60 + int(str(r15)[8:10])*60*60 + int(str(r15)[11:13])*60 + int(str(r15)[14:16])
-			else:
-				walltime = int(str(r15)[6:8])*60*60 + int(str(r15)[9:11])*60 + int(str(r15)[12:14])
-			w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, 0)
-			workload.append(w)
-			id_count += 1
-		line = f_input_before.readline()
-	f_input_before.close()
+# ~ # Read the file first to get a struct to sort by submission time
+# ~ if (MERGE_3_FILES == 1):
+	# ~ line = f_input_before.readline()
+	# ~ while line:
+		# ~ r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15 = line.split()
+		# ~ if (str(r3) == "jobstate=COMPLETED" and int(str(r11)[6:]) <= 20):
+			# ~ if (len(str(r15)) == 17): # Mean that the walltime is superior to 10 days
+				# ~ walltime = int(str(r15)[6:8])*24*60*60 + int(str(r15)[9:11])*60*60 + int(str(r15)[12:14])*60 + int(str(r15)[15:17])
+			# ~ if (len(str(r15)) == 16): # Mean that the walltime is superior to 24h
+				# ~ walltime = int(str(r15)[6:7])*24*60*60 + int(str(r15)[8:10])*60*60 + int(str(r15)[11:13])*60 + int(str(r15)[14:16])
+			# ~ else:
+				# ~ walltime = int(str(r15)[6:8])*60*60 + int(str(r15)[9:11])*60 + int(str(r15)[12:14])
+			# ~ w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, 0)
+			# ~ workload.append(w)
+			# ~ id_count += 1
+		# ~ line = f_input_before.readline()
+	# ~ f_input_before.close()
 	
 line = f_input.readline()
 while line:
@@ -67,33 +66,34 @@ while line:
 			walltime = int(str(r15)[6:7])*24*60*60 + int(str(r15)[8:10])*60*60 + int(str(r15)[11:13])*60 + int(str(r15)[14:16])
 		else:
 			walltime = int(str(r15)[6:8])*60*60 + int(str(r15)[9:11])*60 + int(str(r15)[12:14])
-		w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, 1)
+		w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, -1)
 		workload.append(w)
 		id_count += 1
 	line = f_input.readline()
 f_input.close()
 
-if (MERGE_3_FILES == 1):
-	line = f_input_after.readline()
-	while line:
-		r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15 = line.split()
-		if (str(r3) == "jobstate=COMPLETED" and int(str(r11)[6:]) <= 20):
-			if (len(str(r15)) == 17): # Mean that the walltime is superior to 10 days
-				walltime = int(str(r15)[6:8])*24*60*60 + int(str(r15)[9:11])*60*60 + int(str(r15)[12:14])*60 + int(str(r15)[15:17])
-			if (len(str(r15)) == 16): # Mean that the walltime is superior to 24h
-				walltime = int(str(r15)[6:7])*24*60*60 + int(str(r15)[8:10])*60*60 + int(str(r15)[11:13])*60 + int(str(r15)[14:16])
-			else:
-				walltime = int(str(r15)[6:8])*60*60 + int(str(r15)[9:11])*60 + int(str(r15)[12:14])
-			w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, 2)
-			workload.append(w)
-			id_count += 1
-		line = f_input_after.readline()
-	f_input_after.close()
+# ~ if (MERGE_3_FILES == 1):
+	# ~ line = f_input_after.readline()
+	# ~ while line:
+		# ~ r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15 = line.split()
+		# ~ if (str(r3) == "jobstate=COMPLETED" and int(str(r11)[6:]) <= 20):
+			# ~ if (len(str(r15)) == 17): # Mean that the walltime is superior to 10 days
+				# ~ walltime = int(str(r15)[6:8])*24*60*60 + int(str(r15)[9:11])*60*60 + int(str(r15)[12:14])*60 + int(str(r15)[15:17])
+			# ~ if (len(str(r15)) == 16): # Mean that the walltime is superior to 24h
+				# ~ walltime = int(str(r15)[6:7])*24*60*60 + int(str(r15)[8:10])*60*60 + int(str(r15)[11:13])*60 + int(str(r15)[14:16])
+			# ~ else:
+				# ~ walltime = int(str(r15)[6:8])*60*60 + int(str(r15)[9:11])*60 + int(str(r15)[12:14])
+			# ~ w = Job(int(str(r9)[7:]), int(str(r8)[4:]) - int(str(r7)[6:]), walltime, int(str(r11)[6:]), str(r5)[9:], 0, 0, 2)
+			# ~ workload.append(w)
+			# ~ id_count += 1
+		# ~ line = f_input_after.readline()
+	# ~ f_input_after.close()
 
 # Min sub time takes 0
 workload.sort(key = operator.attrgetter("subtime"))
 min_subtime = workload[0].subtime
-
+nb_jobs_by_phase=id_count/3
+print("There are", id_count - 1, "valid jobs")
 # Getting data. 0 means no data
 f_output = open("inputs/workloads/converted/" + FILENAME, "w")
 if (workload[0].cores >= 5):
@@ -106,6 +106,7 @@ if (workload[0].cores >= 5):
 	elif (r < PROBABILITY_OF_USING_1TB + PROBABILITY_OF_USING_256GB):
 		size = 51.2
 	workload[0].data_size = size*workload[0].cores
+workload[0].workload = 0
 f_output.write("{ id: %d subtime: %d delay: %d walltime: %d cores: %d user: %s data: %d data_size: %f workload: %d }\n" % (1, workload[0].subtime - min_subtime, workload[0].delay, workload[0].walltime, workload[0].cores, workload[0].user, workload[0].data, workload[0].data_size, workload[0].workload))
 last_data = workload[0].data
 last_size = workload[0].data_size
@@ -134,5 +135,11 @@ for i in range (1, id_count - 1):
 			last_user = workload[i].user
 			last_subtime = workload[i].subtime
 			last_core = workload[i].cores
+	if (i < nb_jobs_by_phase):
+		workload[i].workload = 0
+	elif (i < nb_jobs_by_phase*2):
+		workload[i].workload = 1
+	else:
+		workload[i].workload = 2
 	f_output.write("{ id: %d subtime: %d delay: %d walltime: %d cores: %d user: %s data: %d data_size: %f workload: %d }\n" % (i + 1, workload[i].subtime - min_subtime, workload[i].delay, workload[i].walltime, workload[i].cores, workload[i].user, workload[i].data, workload[i].data_size, workload[i].workload))
 f_output.close()
