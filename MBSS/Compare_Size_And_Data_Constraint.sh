936ae61e3f7d71068de36ef78c87319525b7e620
Maxime <maxime.gonthier@ens-lyon.fr>
2022-08-18 17:30:39 +0200

    for testing

--- MBSS/Compare_Size_And_Data_Constraint.sh
+++ MBSS/Compare_Size_And_Data_Constraint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # oarsub -p nova -l core=16,walltime=02:00:00 -r '2022-07-08 12:12:00' "bash Compare_Size_And_Data_Constraint.sh inputs/workloads/converted/2022-01-24-\>2022-01-24 inputs/clusters/rackham_450_128_32_256_4_1024.txt"
+# oarsub -p nova -l core=16,walltime=02:00:00 -r '2022-08-18 16:36:00' "bash Compare_Size_And_Data_Constraint.sh inputs/workloads/converted/2022-03-08-\>2022-03-11 inputs/clusters/rackham_450_128_32_256_4_1024.txt"
 
 start=`date +%s`
 
@@ -20,7 +21,7 @@ CONTRAINTES_TAILLES=1
 
 make -C C/
 
-echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time" > outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+#~ echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time" > outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 
 #~ for ((i=1; i<=9; i++))
 #~ do
@@ -41,18 +42,14 @@ echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue ti
 	#~ cat outputs/Results_${SCHEDULER}.csv >> outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 #~ done
 
-for ((i=1; i<=4; i++))
+#~ for ((i=1; i<=4; i++))
+for ((i=3; i<=4; i++))
 do
 	# Schedulers
-	#~ if [ $((i)) == 1 ]; then SCHEDULER="Fcfs_no_use_bigger_nodes"
-	#~ elif [ $((i)) == 2 ]; then SCHEDULER="Fcfs_no_use_bigger_nodes_easybf"
 	if [ $((i)) == 1 ]; then SCHEDULER="Fcfs"
 	elif [ $((i)) == 2 ]; then SCHEDULER="Fcfs_easybf"
 	elif [ $((i)) == 3 ]; then SCHEDULER="Fcfs_with_a_score_x500_x500_x0_x0"
 	elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_with_a_score_easybf_x500_x500_x0_x0"
-	#~ elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_0_x500_x500_x0_x0"
-	#~ elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_1_x500_x500_x0_x0"
-	#~ elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_area_filling_x500_x500_x0_x1000"
 	fi
 	
 	truncate -s 0 outputs/Results_${SCHEDULER}.csv
