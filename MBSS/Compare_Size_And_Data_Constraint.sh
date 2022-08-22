bf06ebfb570d1c9ea984e430d10d82725dbc287e
Maxime <maxime.gonthier@ens-lyon.fr>
2022-08-22 18:50:42 +0200

    maj

--- MBSS/Compare_Size_And_Data_Constraint.sh
+++ MBSS/Compare_Size_And_Data_Constraint.sh
@@ -21,9 +21,10 @@ CONTRAINTES_TAILLES=1
 
 make -C C/
 
-echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time" > outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+#~ echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time" > outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 
-for ((i=1; i<=9; i++))
+#~ for ((i=1; i<=9; i++))
+for ((i=9; i<=9; i++))
 do
 	# Schedulers
 	if [ $((i)) == 1 ]; then SCHEDULER="Fcfs_no_use_bigger_nodes"
@@ -34,7 +35,7 @@ do
 	elif [ $((i)) == 6 ]; then SCHEDULER="Fcfs_with_a_score_easybf_x500_x500_x0_x0"
 	elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_0_x500_x500_x0_x0"
 	elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_1_x500_x500_x0_x0"
-	elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_area_filling_x500_x500_x0_x1000"
+	elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_area_filling_x500_x500_x0_x20000000"
 	fi
 	
 	truncate -s 0 outputs/Results_${SCHEDULER}.csv
