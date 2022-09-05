cf9b89b2174e335085869f40870d8b898faae7f6
Maxime <maxime.gonthier@ens-lyon.fr>
2022-09-05 14:08:05 +0200

    maj scripts

--- MBSS/Compare_Size_And_Data_Constraint.sh
+++ MBSS/Compare_Size_And_Data_Constraint.sh
@@ -22,31 +22,31 @@ make -C C/
 
 echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time, Mean flow stretch 128 jobs, Mean flow stretch 256 jobs, Mean flow stretch 1024 jobs, Mean flow stretch with a minimum 128 jobs, Mean flow stretch with a minimum 256 jobs, Mean flow stretch with a minimum 1024 jobs" > outputs/Results_Size_And_Data_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 
-for ((i=1; i<=15; i++))
+for ((i=1; i<=14; i++))
 do
 	# Schedulers
 	if [ $((i)) == 1 ]; then SCHEDULER="Fcfs_no_use_bigger_nodes"
 	elif [ $((i)) == 2 ]; then SCHEDULER="Fcfs"
 	elif [ $((i)) == 3 ]; then SCHEDULER="Fcfs_backfill_big_nodes_0"
-	elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_area_filling"
+	#~ elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_area_filling"
 	#~ elif [ $((i)) == 5 ]; then SCHEDULER="Fcfs_area_filling_omniscient"
-	elif [ $((i)) == 5 ]; then SCHEDULER="Fcfs_area_filling_with_ratio"
+	elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_area_filling_with_ratio"
 	#~ elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_area_filling_omniscient_with_ratio"
-	elif [ $((i)) == 6 ]; then SCHEDULER="Fcfs_with_a_score_x1_x0_x0_x0"
-	elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_with_a_score_x500_x500_x0_x0"
+	elif [ $((i)) == 5 ]; then SCHEDULER="Fcfs_with_a_score_x1_x0_x0_x0"
+	elif [ $((i)) == 6 ]; then SCHEDULER="Fcfs_with_a_score_x500_x500_x0_x0"
 	#~ elif [ $((i)) == 10 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_weighted_random_x500_x500_x0_x0"
-	elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_area_filling_with_a_score_x500_x500_x0_x0"
-	#~ elif [ $((i)) == 12 ]; then SCHEDULER="Fcfs_area_filling_omniscient_with_a_score_x500_x500_x0_x0"
-	elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_area_filling_with_ratio_with_a_score_x500_x500_x0_x0"
+	#~ elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_area_filling_with_a_score_x500_x500_x0_x0"
+	elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_area_filling_omniscient_with_a_score_x500_x500_x0_x0"
+	elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_area_filling_with_ratio_with_a_score_x500_x500_x0_x0"
 	#~ elif [ $((i)) == 14 ]; then SCHEDULER="Fcfs_area_filling_with_ratio_7_days_earlier_with_a_score_x500_x500_x0_x0"
 	#~ elif [ $((i)) == 15 ]; then SCHEDULER="Fcfs_area_filling_omniscient_with_ratio_with_a_score_x500_x500_x0_x0"
-	elif [ $((i)) == 10 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_x500_x500_x0_x500"
-	elif [ $((i)) == 11 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_x500_x500_x0_x1000"
-	elif [ $((i)) == 12 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_with_planned_area_x500_x500_x0_x500"
-	elif [ $((i)) == 13 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_with_planned_area_x500_x500_x0_x1000"
+	elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_x500_x500_x0_x500"
+	elif [ $((i)) == 10 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_x500_x500_x0_x1000"
+	elif [ $((i)) == 11 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_with_planned_area_x500_x500_x0_x500"
+	elif [ $((i)) == 12 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_with_planned_area_x500_x500_x0_x1000"
 	#~ elif [ $((i)) == 18 ]; then SCHEDULER="Fcfs_with_a_score_area_factor_with_omniscient_planned_area_x500_x500_x0_x1000"
-	elif [ $((i)) == 14 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_gain_loss_tradeoff_x500_x500_x0_x0"
-	elif [ $((i)) == 15 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_95th_percentile_x500_x500_x0_x0"
+	elif [ $((i)) == 13 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_gain_loss_tradeoff_x500_x500_x0_x0"
+	elif [ $((i)) == 14 ]; then SCHEDULER="Fcfs_with_a_score_backfill_big_nodes_95th_percentile_x500_x500_x0_x0"
 	fi
 	
 	truncate -s 0 outputs/Results_${SCHEDULER}.csv
