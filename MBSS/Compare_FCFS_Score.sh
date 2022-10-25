bd0536996e3e08e2a6ca74d7186d94854d88f911
Maxime <maxime.gonthier@ens-lyon.fr>
2022-10-25 16:53:31 +0200

    plot

--- MBSS/Compare_FCFS_Score.sh
+++ MBSS/Compare_FCFS_Score.sh
@@ -360,80 +360,80 @@ make -C C/
 #~ python3 src/plot_barplot.py Results_FCFS_Score_Adaptative_Multiplier_${WORKLOAD_TP} Mean_Stretch_With_a_Minimum ${CLUSTER_TP} 0 ${OUTPUT_FILE}
 #~ mv ${OUTPUT_FILE} data/Results_FCFS_Score_Adaptative_Multiplier_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 
-# 9. Comparer backfilling 2.0
-OUTPUT_FILE=outputs/Results_FCFS_Score_Backfill_${WORKLOAD_TP}_${CLUSTER_TP}.csv
-if (($((STARTING_I)) == 1))
-then
-	echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time, Mean flow stretch 128 jobs, Mean flow stretch 256 jobs, Mean flow stretch 1024 jobs, Mean flow stretch with a minimum 128 jobs, Mean flow stretch with a minimum 256 jobs, Mean flow stretch with a minimum 1024 jobs" > ${OUTPUT_FILE}
-fi
-for ((i=$((STARTING_I)); i<=20; i++))
-do
-	# Schedulers
-	if [ $((i)) == 1 ]; then SCHEDULER="Fcfs"; BACKFILL_MODE=0
-	elif [ $((i)) == 2 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=0
-	elif [ $((i)) == 3 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=1
-	elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=2
-	elif [ $((i)) == 5 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=3
-	elif [ $((i)) == 6 ]; then SCHEDULER="Fcfs_with_a_score_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
-	elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
-	elif [ $((i)) == 10 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
-	elif [ $((i)) == 11 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 12 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 13 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
-	elif [ $((i)) == 14 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
-	elif [ $((i)) == 15 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
-	elif [ $((i)) == 16 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 17 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
-	elif [ $((i)) == 18 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
-	elif [ $((i)) == 19 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
-	elif [ $((i)) == 20 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
-	fi
-	./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE $BACKFILL_MODE
-done
-echo "Final results are:"
-cat ${OUTPUT_FILE}
-mv ${OUTPUT_FILE} data/Results_FCFS_Score_Backfill_${WORKLOAD_TP}_${CLUSTER_TP}.csv
-
-#~ # 10. Tester data persistence
-#~ OUTPUT_FILE=outputs/Results_Data_Persistence_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+#~ # 9. Comparer backfilling 2.0
+#~ OUTPUT_FILE=outputs/Results_FCFS_Score_Backfill_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 #~ if (($((STARTING_I)) == 1))
 #~ then
 	#~ echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time, Mean flow stretch 128 jobs, Mean flow stretch 256 jobs, Mean flow stretch 1024 jobs, Mean flow stretch with a minimum 128 jobs, Mean flow stretch with a minimum 256 jobs, Mean flow stretch with a minimum 1024 jobs" > ${OUTPUT_FILE}
 #~ fi
-#~ for ((i=$((STARTING_I)); i<=4; i++))
+#~ for ((i=$((STARTING_I)); i<=20; i++))
 #~ do
 	#~ # Schedulers
-	#~ if [ $((i)) == 1 ]; then 
-		#~ SCHEDULER="Fcfs"
-		#~ make -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-		#~ make data_persistence -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-	#~ elif [ $((i)) == 2 ]; then 
-		#~ SCHEDULER="Fcfs_with_a_score_x500_x1_x0_x0"
-		#~ make -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-		#~ make data_persistence -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-	#~ elif [ $((i)) == 3 ]; then 
-		#~ SCHEDULER="Fcfs_with_a_score_mixed_strategy_x500_x1_x0_x0"
-		#~ make -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-		#~ make data_persistence -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-	#~ elif [ $((i)) == 4 ]; then 
-		#~ SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_x500_x1_x0_x0"
-		#~ make -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
-		#~ make data_persistence -C C/
-		#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+	#~ if [ $((i)) == 1 ]; then SCHEDULER="Fcfs"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 2 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 3 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=1
+	#~ elif [ $((i)) == 4 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=2
+	#~ elif [ $((i)) == 5 ]; then SCHEDULER="Fcfs_conservativebf"; BACKFILL_MODE=3
+	#~ elif [ $((i)) == 6 ]; then SCHEDULER="Fcfs_with_a_score_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 7 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 8 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
+	#~ elif [ $((i)) == 9 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
+	#~ elif [ $((i)) == 10 ]; then SCHEDULER="Fcfs_with_a_score_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
+	#~ elif [ $((i)) == 11 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 12 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 13 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
+	#~ elif [ $((i)) == 14 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
+	#~ elif [ $((i)) == 15 ]; then SCHEDULER="Fcfs_with_a_score_mixed_strategy_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
+	#~ elif [ $((i)) == 16 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 17 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=0
+	#~ elif [ $((i)) == 18 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=1
+	#~ elif [ $((i)) == 19 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=2
+	#~ elif [ $((i)) == 20 ]; then SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_conservativebf_x500_x1_x0_x0"; BACKFILL_MODE=3
 	#~ fi
+	#~ ./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE $BACKFILL_MODE
 #~ done
 #~ echo "Final results are:"
 #~ cat ${OUTPUT_FILE}
-#~ mv ${OUTPUT_FILE} data/Results_Data_Persistence_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+#~ mv ${OUTPUT_FILE} data/Results_FCFS_Score_Backfill_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+
+# 10. Tester data persistence
+OUTPUT_FILE=outputs/Results_Data_Persistence_${WORKLOAD_TP}_${CLUSTER_TP}.csv
+if (($((STARTING_I)) == 1))
+then
+	echo "Scheduler,Number of jobs,Maximum queue time,Mean queue time,Total queue time,Maximum flow,Mean flow,Total flow,Transfer time,Makespan,Core time used, Waiting for a load time, Total waiting for a load time and transfer time, Mean Stretch, Mean Stretch With a Minimum, Max Stretch, Max Stretch With a Minimum, Nb Upgraded Jobs, Nb jobs large queue time, Mean flow stretch 128 jobs, Mean flow stretch 256 jobs, Mean flow stretch 1024 jobs, Mean flow stretch with a minimum 128 jobs, Mean flow stretch with a minimum 256 jobs, Mean flow stretch with a minimum 1024 jobs" > ${OUTPUT_FILE}
+fi
+for ((i=$((STARTING_I)); i<=4; i++))
+do
+	# Schedulers
+	if [ $((i)) == 1 ]; then 
+		SCHEDULER="Fcfs"
+		make -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+		make data_persistence -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+	elif [ $((i)) == 2 ]; then 
+		SCHEDULER="Fcfs_with_a_score_x500_x1_x0_x0"
+		make -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+		make data_persistence -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+	elif [ $((i)) == 3 ]; then 
+		SCHEDULER="Fcfs_with_a_score_mixed_strategy_x500_x1_x0_x0"
+		make -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+		make data_persistence -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+	elif [ $((i)) == 4 ]; then 
+		SCHEDULER="Fcfs_with_a_score_adaptative_multiplier_if_EAT_is_t_x500_x1_x0_x0"
+		make -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+		make data_persistence -C C/
+		./C/main $WORKLOAD $CLUSTER $SCHEDULER $CONTRAINTES_TAILLES $OUTPUT_FILE 0
+	fi
+done
+echo "Final results are:"
+cat ${OUTPUT_FILE}
+mv ${OUTPUT_FILE} data/Results_Data_Persistence_${WORKLOAD_TP}_${CLUSTER_TP}.csv
 
 
 end=`date +%s` 
