5a5e2ff0e572e9a1921ca2900b548bb2f1891eff
Maxime <maxime.gonthier@ens-lyon.fr>
2022-12-09 16:24:11 +0100

    corrected plots with crosses

--- MBSS/src/plot_boxplot.py
+++ MBSS/src/plot_boxplot.py
@@ -37,10 +37,26 @@ box = ax.boxplot(columns, patch_artist=True, meanline=True, showmeans=True, whis
 
 c="#095228"
 
+# ~ plt.rcParams['hatch.color'] = "white"
+
 for boxes in box['boxes']:
 	if sys.argv[1] != "outputs/scatter_mean_stretch_all_workloads.csv":
-		boxes.set(color='white', linewidth=1)
-		boxes.set_hatch('/')
+		# ~ boxes.set(color='white', linewidth=1)
+		# ~ boxes.set_edgecolor(color="black")
+		# ~ boxes.set_hatch('/')
+		
+		# ~ boxes.set(color='black', linewidth=2)
+		# change fill color
+		# ~ boxes.set(facecolor = 'green')
+		# change hatch
+		# ~ boxes.set(hatch = '/', color='white')
+		boxes.set(hatch = '/', color="white")
+		# ~ boxes.set(edgecolor = 'black')
+		# ~ boxes.set(hatch = '/', color='white')
+		# ~ boxes.set(color='blue', linewidth=2)
+	# ~ else:
+		# ~ boxes.set_edgecolor(color="black")
+		
 for median in box['medians']:
     # ~ median.set_color('green')
     median.set_color(c)
@@ -59,6 +75,8 @@ print([item.get_ydata()[1] for item in box['whiskers']])
 # ~ print(median)
 # ~ print(mean)
 
+
+
 if sys.argv[1] == "outputs/scatter_mean_stretch_all_workloads.csv":
 	plt.xticks([1, 2, 3, 4], ["EFT", "LEA", "LEO", "LEM"], fontsize=font_size)
 else:
@@ -74,9 +92,12 @@ plt.axhline(y = 1, color = 'black', linestyle = "dotted", linewidth=4)
 # ~ ax.set_ylim(0, 2.5)
 ax.set_ylim(0, 3.15)
 plt.yticks(fontsize=font_size)
-plt.rcParams['hatch.linewidth'] = 5
+
+plt.rcParams['hatch.linewidth'] = 9
+
 for patch, color in zip(box['boxes'], colors):
-    patch.set_facecolor(color)
+    patch.set_facecolor("white")
+    patch.set_edgecolor(color)
     
 if sys.argv[1] == "outputs/scatter_mean_stretch_all_workloads.csv":
 	filename = "plot/Boxplot/box_plot_mean_stretch_all_workloads.pdf"
@@ -86,3 +107,4 @@ else:
 	plt.ylabel('Stretch\'s improvement from FCFS-BF', fontsize=font_size)
 # ~ plt.legend(loc ="upper left")
 plt.savefig(filename)
+
