eb3ecbfe611198d66a442f5a2c291c370836f0d9
MaximeGonthier <maxjp.gr2@gmail.com>
2023-02-16 11:23:10 +0100

    fix

--- MBSS/plot.sh
+++ MBSS/plot.sh
@@ -48,7 +48,7 @@ echo "Workload: ${WORKLOAD_TP} | Model: ${MODEL} | Percentage mode: ${PERCENTAGE
 #~ python3 src/plot_barplot.py Results_${MODEL}_${WORKLOAD_TP} Mean_Stretch_With_a_Minimum_1024 ${CLUSTER_TP} 0 ${DATA_FILE}
 
 # OLD
-#~ python3 src/plot_barplot.py Results_${MODEL}_${WORKLOAD_TP} Mean_Stretch ${CLUSTER_TP} 0 ${DATA_FILE} ${PERCENTAGE_MODE}
+python3 src/plot_barplot.py Results_${MODEL}_${WORKLOAD_TP} Mean_Stretch ${CLUSTER_TP} 0 ${DATA_FILE} ${PERCENTAGE_MODE}
 #~ python3 src/plot_barplot.py Results_${MODEL}_${WORKLOAD_TP} Total_waiting_for_a_load_time_and_transfer_time ${CLUSTER_TP} 0 ${DATA_FILE} ${PERCENTAGE_MODE}
 #~ if [[ ${MODEL} == "FCFS_Score_Backfill" && "$#" == 6 ]]; then
 	#~ python3 src/plot_barplot.py Results_${MODEL}_${WORKLOAD_TP} Number_of_data_reuse ${CLUSTER_TP} 0 ${DATA_FILE} ${PERCENTAGE_MODE} $6
