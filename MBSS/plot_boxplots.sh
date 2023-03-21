caeb366aeef4b4ea75b294dd37db8eef993799cf
MaximeGonthier <maxjp.gr2@gmail.com>
2023-03-21 01:07:13 +0100

    notes, plots, input data, converted workloads, code to get stats from a workload

--- MBSS/plot_boxplots.sh
+++ MBSS/plot_boxplots.sh
@@ -7,8 +7,10 @@ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF transfer_time 0 hist
 python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF core_time 0 boxplot
 python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF core_time 0 hist
 python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF stretch 0 boxplot
+python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF bounded_stretch 0 boxplot
 
 python3 src/plot_boxplot.py ${date1} ${date2} byuser BF transfer_time 0 hist
 python3 src/plot_boxplot.py ${date1} ${date2} byuser BF core_time 0 boxplot
 python3 src/plot_boxplot.py ${date1} ${date2} byuser BF core_time 0 hist
 python3 src/plot_boxplot.py ${date1} ${date2} byuser BF stretch 0 boxplot
+python3 src/plot_boxplot.py ${date1} ${date2} byuser BF bounded_stretch 0 boxplot
