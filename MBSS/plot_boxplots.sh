e080f68f0bce3172aa81b1c1778da682dcb8b0de
MaximeGonthier <maxjp.gr2@gmail.com>
2023-04-03 23:07:26 +0200

    Correcting boxplots

--- MBSS/plot_boxplots.sh
+++ MBSS/plot_boxplots.sh
@@ -6,13 +6,13 @@ date2=$2
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF transfer_time 1 hist
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF core_time 1 boxplot
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF core_time 1 hist
-python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF stretch 0 boxplot
+#~ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF stretch 1 boxplot
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser NO_BF bounded_stretch 1 boxplot
 
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser BF transfer_time 1 hist
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser BF core_time 1 boxplot
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser BF core_time 1 hist
-#~ python3 src/plot_boxplot.py ${date1} ${date2} byuser BF stretch 1 boxplot
+python3 src/plot_boxplot.py ${date1} ${date2} byuser BF stretch 1 boxplot
 #~ python3 src/plot_boxplot.py ${date1} ${date2} byuser BF bounded_stretch 1 boxplot
 
 # ECDF
