2f27432b30435daa254d5dadab63682437749cc0
Maxime <maxime.gonthier@ens-lyon.fr>
2024-03-18 16:45:43 -0500

    maj

--- Simulator/plot_all.sh
+++ Simulator/plot_all.sh
@@ -2,9 +2,9 @@
 
 # bash plot_all.sh
 
-bash test.sh set_of_endpoints_1 set_of_endpoints_1_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
-bash test.sh set_of_endpoints_2 set_of_endpoints_2_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
-bash test.sh set_of_endpoints_3 set_of_endpoints_3_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
+bash test.sh set_of_endpoints_1 meggie_and_emmy 10 1 count_from_database
+bash test.sh set_of_endpoints_2 meggie_and_emmy 10 1 count_from_database
+bash test.sh set_of_endpoints_3 meggie_and_emmy 10 1 count_from_database
 
 echo ""
 echo "All done :D"
