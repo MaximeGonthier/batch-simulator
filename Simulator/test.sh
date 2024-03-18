ebf90153c9d09490833f458c322b24adf17bd687
Maxime <maxime.gonthier@ens-lyon.fr>
2024-03-18 14:08:53 -0500

    ùa

--- Simulator/test.sh
+++ Simulator/test.sh
@@ -9,10 +9,9 @@
 # bash test.sh set_of_endpoints_1 set_of_endpoints_1_9users_nocoresmax_meggie_and_emmy 9 1 default
 
 # In paper
-# bash test.sh set_of_endpoints_1 set_of_endpoints_1_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
-# bash test.sh set_of_endpoints_2 set_of_endpoints_2_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
-# bash test.sh set_of_endpoints_3 set_of_endpoints_3_9users_nocoresmax_meggie_and_emmy 9 1 count_from_database
-
+# bash test.sh set_of_endpoints_1 meggie_and_emmy 10 1 count_from_database
+# bash test.sh set_of_endpoints_2 meggie_and_emmy 10 1 count_from_database
+# bash test.sh set_of_endpoints_3 meggie_and_emmy 10 1 count_from_database
 
 endpoints=$1
 workload=$2
@@ -43,51 +42,51 @@ echo ""
 
 
 # With emmy and meggie database
-python3 src/write_workload.py inputs/workloads/converted/${workload}_${mode_for_repetition} 1 ${mode_for_repetition} inputs/workloads/meggie_and_emmy-job-trace-extrapolated.csv
+#~ python3 src/write_workload.py inputs/workloads/converted/${workload}_${mode_for_repetition} 1 ${mode_for_repetition} inputs/workloads/meggie_and_emmy-job-trace-extrapolated.csv
 
 n_iteration=1
 if [ ${endpoints} == set_of_endpoints_1 ]; then
-./src/main inputs/workloads/converted/${workload}_${mode_for_repetition} inputs/clusters/${endpoints} no_schedule 0 outputs/${workload}_${mode_for_repetition}_credit.csv 0 100 ${nusers} from_emmy_and_meggie credit
+./src/main inputs/workloads/converted/${workload}_${mode_for_repetition} inputs/clusters/${endpoints} no_schedule 0 outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv 0 100 ${nusers} from_emmy_and_meggie credit
 fi
-./src/main inputs/workloads/converted/${workload}_${mode_for_repetition} inputs/clusters/${endpoints} no_schedule 0 outputs/${workload}_${mode_for_repetition}_carbon.csv 0 100 ${nusers} from_emmy_and_meggie carbon
+./src/main inputs/workloads/converted/${workload}_${mode_for_repetition} inputs/clusters/${endpoints} no_schedule 0 outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv 0 100 ${nusers} from_emmy_and_meggie carbon
 
 echo ""
 echo "Plot barplots"
 
 if [ ${endpoints} == set_of_endpoints_1 ]; then
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "total_energy" ${n_iteration} credit
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "nb_jobs_completed" ${n_iteration} credit
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "nb_jobs_completed_in_mean_core_hours" ${n_iteration} credit
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "queue_time" ${n_iteration} credit
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "carbon_used" ${n_iteration} credit
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "total_energy" ${n_iteration} credit
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "nb_jobs_completed" ${n_iteration} credit
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "nb_jobs_completed_in_mean_core_hours" ${n_iteration} credit
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "queue_time" ${n_iteration} credit
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "carbon_used" ${n_iteration} credit
 fi
 
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "total_energy" ${n_iteration} carbon
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "nb_jobs_completed" ${n_iteration} carbon
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "nb_jobs_completed_in_mean_core_hours" ${n_iteration} carbon
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "queue_time" ${n_iteration} carbon
-python3 src/plot_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "carbon_used" ${n_iteration} carbon
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "total_energy" ${n_iteration} carbon
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "nb_jobs_completed" ${n_iteration} carbon
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "nb_jobs_completed_in_mean_core_hours" ${n_iteration} carbon
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "queue_time" ${n_iteration} carbon
+python3 src/plot_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "carbon_used" ${n_iteration} carbon
 
 echo ""
 echo "Plot curves"
 
 if [ ${endpoints} == set_of_endpoints_1 ]; then
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "finish_times" ${n_iteration} credit
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "finish_times_core_hours_Y_axis" ${n_iteration} credit
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "energy_consumed" ${n_iteration} credit
-# python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "finish_times_core_hours_Y_axis_energy_consumed_X_axis" ${n_iteration} credit
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "finish_times" ${n_iteration} credit
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "finish_times_core_hours_Y_axis" ${n_iteration} credit
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "energy_consumed" ${n_iteration} credit
+# python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "finish_times_core_hours_Y_axis_energy_consumed_X_axis" ${n_iteration} credit
 fi
 
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "finish_times" ${n_iteration} carbon
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "finish_times_core_hours_Y_axis" ${n_iteration} carbon
-python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "energy_consumed" ${n_iteration} carbon
-# python3 src/plot_curve.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "finish_times_core_hours_Y_axis_energy_consumed_X_axis" ${n_iteration} carbon
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "finish_times" ${n_iteration} carbon
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "finish_times_core_hours_Y_axis" ${n_iteration} carbon
+python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "energy_consumed" ${n_iteration} carbon
+# python3 src/plot_curve.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "finish_times_core_hours_Y_axis_energy_consumed_X_axis" ${n_iteration} carbon
 
 echo ""
 echo "Plot stacked barplots"
 
 if [ ${endpoints} == set_of_endpoints_1 ]; then
-python3 src/plot_stacked_barplots.py outputs/${workload}_${mode_for_repetition}_credit.csv ${nusers} ${workload}_${mode_for_repetition}_credit "machine_used" ${n_iteration} credit
+python3 src/plot_stacked_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_credit.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_credit "machine_used" ${n_iteration} credit
 fi
 
-python3 src/plot_stacked_barplots.py outputs/${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${workload}_${mode_for_repetition}_carbon "machine_used" ${n_iteration} carbon
+python3 src/plot_stacked_barplots.py outputs/${endpoints}_${workload}_${mode_for_repetition}_carbon.csv ${nusers} ${endpoints}_${workload}_${mode_for_repetition}_carbon "machine_used" ${n_iteration} carbon
