50d5df614d528ef80daeb1947b8d3fe807aa2880
Alok Kamatar <alokvk2@uchicago.edu>
2025-07-14 14:40:48 -0500

    Adding missing files from last year

---
+++ green-access-simulator/write_workload_all.sh
@@ -0,0 +1,6 @@
+workload="meggie_and_emmy"
+nusers=10
+N_cores_max=1
+mode_for_repetition="count_from_database"
+
+python scripts/write_workload.py inputs/workloads/converted/${workload}_${mode_for_repetition} 1 ${mode_for_repetition} inputs/workloads/meggie_and_emmy-job-trace-extrapolated.csv
