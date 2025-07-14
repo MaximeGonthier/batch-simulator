72b388395601ec24d73ff879e63f14239470e2af
gonthier <maxjp.gr@gmail.com>
2025-07-14 16:04:14 -0500

    script for table6

--- Simulator/plot_all.sh
+++ Simulator/plot_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#~ bash test.sh set_of_endpoints_1 meggie_and_emmy 10 1 count_from_database
+bash test.sh set_of_endpoints_1 meggie_and_emmy 10 1 count_from_database
 bash test.sh set_of_endpoints_2 meggie_and_emmy 10 1 count_from_database
 
 echo ""
