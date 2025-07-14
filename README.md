e5ad51f75badfb87d88aba93c2a6c0472297d45f
gonthier <maxjp.gr@gmail.com>
2025-07-14 14:59:47 -0500

    update for parsing

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
 # Batch simulator for energy incentive
-
-This repository is a modification of the repository https://gitlab.inria.fr/mgonthie/data-aware-batch-scheduling made on the 8 Feb 2024.
-It does not contain all of the things related to the project conducted in the previous repo like scripts, plots, articles and so on.
-
-This simulator aims at improving the one from https://gitlab.inria.fr/mgonthie/data-aware-batch-scheduling to include functionalitites like heterogeneous set of nodes, energy consumption of a job on a node, incentivize users to be more energy efficient and simulate the behavior of users with set goals (speed, energy, mix of both) and give an energy credit to each users.
-
-This repo introduce the no_schedule "scheduler" which let user choose an endpoint.
-
-We only have jobs that use all the nodes (as they are machines in our case)
-
-In the code, all the modification are marked with /** ENERGY INCENTIVE **/
-
-Added the number of cpu, idle power and cpu tdp ionto the info of the machines
-Added duration_on_machine: 100 120 34 59  in the workload data, much correspond to the number of machines!
-Same for energy_on_machine
-
-total_number_jobs must be a multiple of nusers
--- Simulator/src/parse_emmy_file_to_get_runtime_energy_data.py
+++ Simulator/src/parse_emmy_file_to_get_runtime_energy_data.py
@@ -1,17 +1,3 @@
-# Used to parse data from https://zenodo.org/records/3666632 files named emmy
-
-# python3 src/parse_emmy_file_to_get_runtime_energy_data.py emmy_job_input_file emmy_power_input_file
-# python3 src/parse_emmy_file_to_get_runtime_energy_data.py ../../ipdps20-paper-hpc-power-traces/data/cluster_characteristics/emmy/job_traces/rrze-traces-emmy-jobs-01.txt ../../ipdps20-paper-hpc-power-traces/data/cluster_characteristics/emmy/pwr_traces/rrze-traces-emmy-avg-pwr-01.txt inputs/workloads/emmy-1-raw
-
-# Job input file look like this
-# ~ 01/01/2019 00:01:33;Q;1033693.eadm;queue=route
-# ~ 01/01/2019 00:01:33;Q;1033693.eadm;queue=devel
-# ~ 01/01/2019 00:01:44;S;1033693.eadm Resource_List.nodes=4:ppn=40 Resource_List.walltime=00:59:00
-# ~ 01/01/2019 00:28:07;E;1033693.eadm Resource_List.nodes=4:ppn=40 Resource_List.walltime=00:59:00 resources_used.walltime=00:26:03 account=e9f5a27c jobstr=362c9816
-
-# Energy input file look like this
-# ~ 1033693  average power consumption (CPU+DRAM)      :      163.7 W
-
 import sys
 import pandas as pd
 
@@ -27,7 +13,6 @@ total_number_of_jobs = 0
 total_number_of_jobs_with_energy = 0
 
 print("Job input file:", job_input_file)
-print("Energy input file:", energy_input_file)
 
 f = open(output_file, "w")
 f.write("Power used per node, Job Id, Number of nodes used, Number of cores used, Requested Walltime, Runtime, Username\n")
@@ -42,9 +27,7 @@ with open(job_input_file, 'r') as file:
 						check_next_char_for_more_than_9_nodes = False
 						if (char != ":"):
 							nb_nodes = nb_nodes*10 + int(char)
-						# ~ print("Nb of nodes:", nb_nodes)
 						f.write(str(nb_nodes) + ", ")
-						# ~ print("Nb of cores:", nb_nodes*20)
 						f.write(str(nb_nodes*20) + ", ")
 						break
 					if get_nb_node_next_char == True:
@@ -57,7 +40,6 @@ with open(job_input_file, 'r') as file:
 						get_nb_node_next_char = True
 			for char in word:
 				if (char == "E"): # A terminated job, so getting info on number of nodes
-					# ~ print(line)
 					total_number_of_jobs += 1
 					get_nb_node_next_word = True
 					i = 11
@@ -78,48 +60,24 @@ with open(job_input_file, 'r') as file:
 									if (str(float(energy_used)) == "nan"):
 										found = False
 										break
-									# ~ print("Energy used int:", float(energy_used), "Watts")
-									# ~ print("Energy used:", energy_used, "Watts")
 									f.write(str(float(energy_used)) + ", ")
 									break
 					if found == False:
 						break
 					else:
-						# ~ print("Job Id:", job_id)
 						f.write(str(job_id) + ", ")
 					file_power.close()
 					break
 			if (word[0:23] == "Resource_List.walltime=" and found == True): # Getting info on runtime
 				walltime = int(word[23])*10*3600 + int(word[24])*3600 + int(word[26])*10*60 + int(word[27])*60 + int(word[29])*10 + int(word[30])
-				# ~ print("Walltime:", walltime, "Seconds")
 				f.write(str(walltime) + ", ")
 			if (word[0:24] == "resources_used.walltime=" and found == True): # Getting info on runtime
 				runtime = int(word[24])*10*3600 + int(word[25])*3600 + int(word[27])*10*60 + int(word[28])*60 + int(word[30])*10 + int(word[31])
-				# ~ print("Runtime:", runtime, "Seconds")
 				f.write(str(runtime) + ", ")
 			if (word[0:8] == "account=" and found == True): # Getting info on runtime
 				username = word[8:17]
-				# ~ print("Username:", username)
 				f.write(str(username) + "\n")
 				found = False
 				
 file.close
 f.close
-
-print(total_number_of_jobs_with_energy, "/", total_number_of_jobs)
-# ~ Application_name = list(df.iloc[:, 8])
-# ~ Nlines = len(Application_name) # Number of lines in the file without the header
-# ~ Endpoint_used = list(df.iloc[:, 16])
-# ~ Endpoint_status = list(df.iloc[:, 12]) # WARM or WARMING, need to ignore WARMING
-# ~ Energy = list(df.iloc[:, 13])
-# ~ Runtime = list(df.iloc[:, 14])
-
-# ~ nruns = 0
-# ~ total_energy = 0
-# ~ total_runtime = 0
-# ~ current_application_name = ""
-# ~ current_endpoint = ""
-# ~ for i in range(0, Nlines):
-
-# Printing the last read values
-# ~ print(current_application_name, "-", current_endpoint, "-", total_energy/nruns, "-", total_runtime/nruns)
